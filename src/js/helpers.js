eaeb6241a15a4d1d24c6e14a51ca81e582d5c3f3
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-12-04 12:23:19 +0300

    final

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,5 +1,5 @@
 // Функция возвращает случайное число в заданном диапазоне
-// По этому числу мы будем выбирать фигуры
+// По возвращенному числу мы будем выбирать фигуры
 export const getRandom = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -7,7 +7,7 @@ export const getRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-// Функция поворота матрицы на 90 градусов. Нужна для поворота фигур
+// Функция поворота матрицы на 90 градусов.
 export const rotate = (matrix) => {
   const N = matrix.length - 1;
   const result = matrix.map((row, i) => row.map((val, j) => matrix[N - j][i]));
--- src/js/main.js
+++ src/js/main.js
@@ -28,7 +28,6 @@ let figure;
 // Заполним массив пустыми ячейками
 // Чтобы у фигур при заполнении всего поля не было наложения друг на друга - несколько строк оставим за видимой областью игрового поля
 const prepareField = () => {
-  // playfield
   for (let row = -2; row < 20; row++) {
     playfield[row] = [];
 
@@ -40,16 +39,13 @@ const prepareField = () => {
 
 // Функция для завершения игры
 const showGameOver = () => {
-  // rAF, gameOver, context, playingField,
-  // прекращаем всю анимацию игры
+  // Прекращение анимации игры
   cancelAnimationFrame(rAF);
-  // ставим флаг окончания
+  //  Флаг окончания игры
   gameOver = true;
-  // рисуем чёрный прямоугольник посередине поля
   context.fillStyle = "black";
   context.globalAlpha = 0.75;
   context.fillRect(0, playingField.height / 2 - 30, playingField.width, 60);
-  // пишем надпись белым моноширинным шрифтом по центру
   context.globalAlpha = 1;
   context.fillStyle = "white";
   context.font = "36px monospace";
@@ -64,7 +60,6 @@ const showGameOver = () => {
 
 // Создадим случайную последовательность фигур, которая появится в игре
 const generateSequence = () => {
-  // figuresSequence
   const sequence = ["I", "J", "L", "O", "S", "T", "Z"];
 
   while (sequence.length) {
@@ -77,14 +72,13 @@ const generateSequence = () => {
 
 // получаем следующую фигуру
 const getNextFigure = () => {
-  // figuresSequence, figures, playfield,
   // Если массив пустой - сгенерируем случайную фигуру
   if (figuresSequence.length === 0) {
     generateSequence();
   }
-  // берём первую фигуру из массива
+  // Берём первую фигуру из массива
   const name = figuresSequence.pop();
-  // сразу создаём матрицу, с которой мы отрисуем фигуру
+  // Сразу создаём матрицу, с которой мы отрисуем фигуру
   const matrix = figures[name];
 
   // I и O стартуют с середины, остальные — чуть левее
@@ -93,7 +87,6 @@ const getNextFigure = () => {
   // I начинает с 21 строки (смещение -1), а все остальные — со строки 22 (смещение -2)
   const row = name === "I" ? -1 : -2;
 
-  // вот что возвращает функция
   return {
     name: name,
     matrix: matrix,
@@ -104,8 +97,7 @@ const getNextFigure = () => {
 
 // проверяем после появления или вращения, может ли матрица (фигура) быть в этом месте поля или она вылезет за его границы
 const isValidMove = (matrix, cellRow, cellCol) => {
-  // playfield
-  // проверяем все строки и столбцы
+  // Проверим все строки и столбцы
   for (let row = 0; row < matrix.length; row++) {
     for (let col = 0; col < matrix[row].length; col++) {
       if (
@@ -121,30 +113,29 @@ const isValidMove = (matrix, cellRow, cellCol) => {
       }
     }
   }
-  // а если мы дошли до этого момента и не закончили раньше — то всё в порядке
+  // Если мы дошли до этого момента и не закончили раньше — то всё в порядке
   return true;
 };
 
-// когда фигура окончательна встала на своё место
+// Когда фигура окончательна встала на своё место
 const placeFigure = () => {
-  // figure, playfield, totalScore, score,
-  // обрабатываем все строки и столбцы в игровом поле
+  // Обрабатываем все строки и столбцы в игровом поле
   for (let row = 0; row < figure.matrix.length; row++) {
     for (let col = 0; col < figure.matrix[row].length; col++) {
       if (figure.matrix[row][col]) {
-        // если край фигуры после установки вылезает за границы поля, то игра закончилась
+        // Если край фигуры после установки вылезает за границы поля, то игра закончилась
         if (figure.row + row < 0) {
           return showGameOver();
         }
-        // если всё в порядке, то записываем в массив игрового поля нашу фигуру
+        // Если всё в порядке, то записываем в массив игрового поля нашу фигуру
         playfield[figure.row + row][figure.col + col] = figure.name;
       }
     }
   }
 
-  // проверяем, чтобы заполненные ряды очистились снизу вверх
+  // Проверим, чтобы заполненные ряды очистились снизу вверх
   for (let row = playfield.length - 1; row >= 0; ) {
-    // если ряд заполнен
+    // если ряд заполнен -
     if (playfield[row].every((cell) => !!cell)) {
       totalScore += 10;
       score.textContent = `Счет: ${totalScore}`;
@@ -156,21 +147,20 @@ const placeFigure = () => {
         }
       }
     } else {
-      // переходим к следующему ряду
+      // и переходим к следующему ряду
       row--;
     }
   }
-  // получаем следующую фигуру
+  // Получим следующую фигуру
   figure = getNextFigure();
 };
 
-// следим за нажатиями на клавиши
+// Обработчик события нажатия на клавиши
 document.addEventListener("keydown", function (e) {
-  // gameOver, figure,
-  // если игра закончилась — сразу выходим
+  // Если игра закончилась — сразу остановим игру
   if (gameOver) return;
 
-  // стрелки влево и вправо
+  // кнопки влево и вправо
   if (e.which === 37 || e.which === 39) {
     const col =
       e.which === 37
@@ -178,7 +168,7 @@ document.addEventListener("keydown", function (e) {
           figure.col - 1
         : figure.col + 1;
 
-    // если так ходить можно, то запоминаем текущее положение
+    // Если так можно - запоминаем текущее положение
     if (isValidMove(figure.matrix, figure.row, col)) {
       figure.col = col;
     }
@@ -188,7 +178,7 @@ document.addEventListener("keydown", function (e) {
   if (e.which === 38) {
     // поворачиваем фигуру на 90 градусов
     const matrix = rotate(figure.matrix);
-    // если так ходить можно — запоминаем
+    // Если так можно - запоминаем текущее положение
     if (isValidMove(matrix, figure.row, figure.col)) {
       figure.matrix = matrix;
     }
@@ -198,61 +188,58 @@ document.addEventListener("keydown", function (e) {
   if (e.which === 40) {
     // смещаем фигуру на строку вниз
     const row = figure.row + 1;
-    // если опускаться больше некуда — запоминаем новое положение
+    // Если опускаться больше некуда — запоминаем новое положение
     if (!isValidMove(figure.matrix, row, figure.col)) {
       figure.row = row - 1;
-      // ставим на место и смотрим на заполненные ряды
+      // Ставим на место и смотрим на заполненные ряды
       placeFigure();
       return;
     }
-    // запоминаем строку, куда стала фигура
+    // Запоминаем строку, куда стала фигура
     figure.row = row;
   }
 });
 
-// главный цикл игры
+// Главный цикл игры
 const loop = () => {
-  // rAF, context, playingField, playfield, colors, square, figure, framesCount
   // начинаем анимацию
   rAF = requestAnimationFrame(loop);
-  // очищаем холст
+  // Очищаем холст
   context.clearRect(0, 0, playingField.width, playingField.height);
 
-  // рисуем игровое поле с учётом заполненных фигур
+  // Рисуем игровое поле с учётом заполненных фигур
   for (let row = 0; row < 20; row++) {
     for (let col = 0; col < 10; col++) {
       if (playfield[row][col]) {
         const name = playfield[row][col];
         context.fillStyle = colors[name];
 
-        // рисуем всё на один пиксель меньше, чтобы получился эффект «в клетку»
+        // Рисуем всё на один пиксель меньше, чтобы получился эффект «в клетку»
         context.fillRect(col * square, row * square, square - 1, square - 1);
       }
     }
   }
 
-  // рисуем текущую фигуру
+  // Рисуем текущую фигуру
   if (figure) {
-    // фигура сдвигается вниз каждые 35 кадров
+    // Фигура сдвигается вниз каждые 35 кадров
     if (++framesCount > 35) {
       figure.row++;
       framesCount = 0;
 
-      // если движение закончилось — рисуем фигуру в поле и проверяем, можно ли удалить строки
+      // Если движение закончилось — рисуем фигуру в поле и проверяем, можно ли удалить строки
       if (!isValidMove(figure.matrix, figure.row, figure.col)) {
         figure.row--;
         placeFigure();
       }
     }
-
-    // не забываем про цвет текущей фигуры
     context.fillStyle = colors[figure.name];
 
-    // отрисовываем её
+    // Отрисовываем её
     for (let row = 0; row < figure.matrix.length; row++) {
       for (let col = 0; col < figure.matrix[row].length; col++) {
         if (figure.matrix[row][col]) {
-          // и снова рисуем на один пиксель меньше
+          // И снова рисуем на один пиксель меньше
           context.fillRect(
             (figure.col + col) * square,
             (figure.row + row) * square,
@@ -265,7 +252,7 @@ const loop = () => {
   }
 };
 
-// старт игры
+// Старт игры
 start.addEventListener("click", () => {
   playfield = [];
   figuresSequence = [];
